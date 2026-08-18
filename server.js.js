b7fc76c30cd727aaddec843c3e63e07e84592c61
joserage7w7-hue <joserage7w7@gmail.com>
2026-08-17 23:32:11 -0600

    Add files via upload

--- server.js.js
+++ server.js.js
@@ -5,8 +5,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => res.send("Aiko backend OK"));
 
 app.post("/aiko", (req, res) => {
