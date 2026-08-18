9669fa2b78a68de3a7509fe134d682b3112ee223
joserage7w7-hue <joserage7w7@gmail.com>
2026-08-17 23:43:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// IMPORTANTE en Render:
 const PORT = process.env.PORT || 3000;
 
-// Ruta raíz para probar en el navegador
 app.get("/", (req, res) => {
   res.send("Aiko backend OK");
 });
 
-// Endpoint que usará Roblox
 app.post("/aiko", (req, res) => {
   const message = (req.body?.message || "").toString().slice(0, 200);
   res.json({ reply: `Ayyy "${message}"... ven acá 😤💗` });
